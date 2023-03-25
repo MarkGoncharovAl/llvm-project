4018725463dcbfb852f62ace779e53f513cbd8a6
MarkGoncharovAl <goncharov.ma@phystech.edu>
2023-03-25 13:18:14 +0300

    first attempt again

--- llvm/include/llvm/TargetParser/Triple.h
+++ llvm/include/llvm/TargetParser/Triple.h
@@ -52,6 +52,7 @@ public:
     aarch64_be,     // AArch64 (big endian): aarch64_be
     aarch64_32,     // AArch64 (little endian) ILP32: aarch64_32
     arc,            // ARC: Synopsys ARC
+    MAG,            // MAG: Mark Goncharov's ARC
     avr,            // AVR: Atmel AVR microcontroller
     bpfel,          // eBPF or extended BPF or 64-bit BPF (little endian)
     bpfeb,          // eBPF or extended BPF or 64-bit BPF (big endian)
